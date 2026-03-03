8ecefb89cf90b8563f9ebd31582df048f9c3d968
raheem <raheemideadev@gmail.com>
2026-03-03 17:13:58 +0500

    Implemented Material3 expressive motion;

--- app/src/main/java/xyz/teamgravity/m3expressivemotion/MainActivity.kt
+++ app/src/main/java/xyz/teamgravity/m3expressivemotion/MainActivity.kt
@@ -4,9 +4,25 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.semantics.Role
+import androidx.compose.ui.unit.dp
 import xyz.teamgravity.m3expressivemotion.ui.theme.M3ExpressiveMotionTheme
 
 class MainActivity : ComponentActivity() {
@@ -16,6 +32,34 @@ class MainActivity : ComponentActivity() {
         setContent {
             M3ExpressiveMotionTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { padding ->
+                    Box(
+                        contentAlignment = Alignment.Center,
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(padding)
+                    ) {
+                        var scaledDown by remember { mutableStateOf(true) }
+                        val scaleProgress by animateFloatAsState(
+                            targetValue = if (scaledDown) 0F else 1F,
+                            animationSpec = MaterialTheme.motionScheme.fastSpatialSpec()
+                        )
+
+                        Box(
+                            modifier = Modifier
+                                .size(100.dp)
+                                .graphicsLayer {
+                                    scaleX = 1F + scaleProgress
+                                    scaleY = 1F + scaleProgress
+                                }
+                                .background(Color.Red)
+                                .clickable(
+                                    role = Role.Button,
+                                    onClick = {
+                                        scaledDown = !scaledDown
+                                    }
+                                )
+                        )
+                    }
                 }
             }
         }
--- app/src/main/java/xyz/teamgravity/m3expressivemotion/ui/theme/Theme.kt
+++ app/src/main/java/xyz/teamgravity/m3expressivemotion/ui/theme/Theme.kt
@@ -1,9 +1,9 @@
 package xyz.teamgravity.m3expressivemotion.ui.theme
 
-import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.MotionScheme
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
@@ -21,22 +21,11 @@ private val LightColorScheme = lightColorScheme(
     primary = Purple40,
     secondary = PurpleGrey40,
     tertiary = Pink40
-
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
 )
 
 @Composable
 fun M3ExpressiveMotionTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
-    // Dynamic color is available on Android 12+
     dynamicColor: Boolean = true,
     content: @Composable () -> Unit
 ) {
@@ -52,7 +41,7 @@ fun M3ExpressiveMotionTheme(
 
     MaterialTheme(
         colorScheme = colorScheme,
-        typography = Typography,
+        motionScheme = MotionScheme.expressive(),
         content = content
     )
 }
