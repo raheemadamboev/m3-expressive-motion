539d5afc2e844825c785cd903118a33c186e4324
raheem <raheemideadev@gmail.com>
2026-03-03 17:01:41 +0500

    Installed dependencies;

--- app/src/main/java/xyz/teamgravity/m3expressivemotion/MainActivity.kt
+++ app/src/main/java/xyz/teamgravity/m3expressivemotion/MainActivity.kt
@@ -5,12 +5,8 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import xyz.teamgravity.m3expressivemotion.ui.theme.M3ExpressiveMotionTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +15,9 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             M3ExpressiveMotionTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Scaffold(modifier = Modifier.fillMaxSize()) { padding ->
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    M3ExpressiveMotionTheme {
-        Greeting("Android")
     }
 }
